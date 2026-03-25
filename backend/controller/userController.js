3af46df9472281b8548a102544c2606cacf4e73f
Onesmus Wambua <onesmuswambua747@gmail.com>
2026-03-26 02:20:48 +0300

    added authorization header option cookies

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -44,7 +44,7 @@ export const signIn = async (req, res) => {
     res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "Lax",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
     path: "/", // 🔥 important
     maxAge: 2 * 24 * 60 * 60 * 1000,
   });
