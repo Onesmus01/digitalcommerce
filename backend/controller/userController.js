1bb9ba9049d2c390e634c3b660f62452929c9585
Onesmus Wambua <onesmuswambua747@gmail.com>
2026-03-25 22:01:04 +0300

    backend domain fix

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -45,7 +45,6 @@ export const signIn = async (req, res) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
-    domain: ".onrender.com",
     path: "/", // 🔥 important
     maxAge: 2 * 24 * 60 * 60 * 1000,
   });
